b3aa774af51777e6991e9616c18b60faf58f848f
John Napier <jcee.napier@gmail.com>
2022-03-28 18:02:01 -0600

    Add passing test for dashboard successful metrics GET

--- one_minute_writer_dashboard/dashboard/tests/test_dashboard_get.py
+++ one_minute_writer_dashboard/dashboard/tests/test_dashboard_get.py
@@ -7,10 +7,20 @@ from dashboard.models import WritingInfo
 from dashboard.serializers import DashboardMetricsSerializer
 
 CREATE_DASHBOARD_URL = reverse('dashboard:dashboard_list')
-# import ipdb; ipdb. set_trace() 
 
-def test_get_dashboard_metrics_success(self):
-  """Test retreiving dashboard metrics with valid payload is successful"""
-  payload = {
-    
-  }
+class PublicDashboardAPITests(TestCase):
+
+  def setUp(self): 
+    self.client = APIClient()
+
+  def test_get_dashboard_metrics_success(self):
+    """Test retreiving dashboard metrics with valid payload is successful"""
+    WritingInfo.objects.create(id = 1, writing_id = 1, word_count = 100, time_spent = 200,)
+     
+    payload = {
+      'writing_ids': '1',
+    }
+
+    response = self.client.get(CREATE_DASHBOARD_URL, payload)
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
