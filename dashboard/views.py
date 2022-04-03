65e2eeb2dfae83ec89c242c4f02ab1e98a123dfe
John Napier <jcee.napier@gmail.com>
2022-04-03 14:27:10 -0600

    Comment out Python Pry

--- dashboard/views.py
+++ dashboard/views.py
@@ -78,6 +78,7 @@ def dashboard_list(request):
     elif request.method == 'POST':
         # Capture the posted ID
         # id = request.GET.get('writing_id', '')
+        # import ipdb; ipdb.set_trace()
         id = request.POST.get('writing_id', '')
         id_list = StringManipulation.char_split(id)
         
@@ -91,7 +92,6 @@ def dashboard_list(request):
 
             if entries_list == []:
                 # If this is a new document ID, no calculations need to be done. Simply grab the word count, the time, and the ID and persist them to the DB.
-                # import ipdb; ipdb.set_trace()
                 # first_word_count = request.GET.get('word_count', '')
                 # first_total_time = request.GET.get('total_time', '')
                 first_word_count = request.POST.get('word_count', '')
